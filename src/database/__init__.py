9dd7bbc5a82cea7645a8a74d023a2bb9c64e2ba2
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-14 17:17:26 +0300

    feat: add engine and get_async_session

---
+++ src/database/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["get_async_session"]
+
+from src.database.db import get_async_session
---
+++ src/database/db.py
@@ -0,0 +1,22 @@
+import logging
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from src.config import settings
+
+logger = logging.getLogger(__name__)
+
+async_engine = create_async_engine(url=settings.db.URL.get_secret_value())
+
+async_session_maker = async_sessionmaker(
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    try:
+        async with async_session_maker() as session:
+            yield session
+    except OSError as e:
+        logger.error(f"Database connection failed: {e}")
