90008d441739c0a5c7684dc57a697b80875f4fbe
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-15 18:55:23 +0300

    feat: add async_session_maker

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["get_async_session"]
+__all__ = ["get_async_session", "async_session_maker"]
 
-from src.database.db import get_async_session
+from src.database.db import get_async_session, async_session_maker
