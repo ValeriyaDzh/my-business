cc2fcd5a3f36f31cc20967fa7e48aa26e73e9e0f
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-15 19:02:59 +0300

    feat: add TokenService

---
+++ src/utils/auth/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["TokenService", "Password"]
+
+from src.utils.auth.token import TokenService
+from src.utils.auth.utils import Password
---
+++ src/utils/auth/token.py
@@ -0,0 +1,28 @@
+import random
+import logging
+
+from redis import Redis
+
+from src.config import settings
+
+logger = logging.getLogger(__name__)
+
+
+class TokenService:
+
+    cache: Redis = settings.cache.REDIS
+
+    def generate_signup_token(self):
+        return random.randint(1000, 9999)
+
+    def save_signup_token(self, account: str, token: int, ttl: int = 300):
+        self.cache.setex(account, ttl, token)
+
+    def verify_signup_token(self, account: str, token: int) -> bool:
+        saved_token = self.cache.get(account)
+        logger.debug(f"{saved_token}")
+        if saved_token and int(saved_token) == token:
+            self.cache.delete(account)
+            logger.debug(f"Email {account} has been verified")
+            return True
+        return False
