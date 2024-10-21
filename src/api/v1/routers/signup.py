a6ad20da86a81d041dc363218d13a856ecf09f71
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-21 12:54:21 +0300

    feat: add sign_up_complete

--- src/api/v1/routers/signup.py
+++ src/api/v1/routers/signup.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from pydantic import EmailStr
 
-from src.schemas.signup import SignUp, Message, VerifyEmail
+from src.schemas.signup import SignUp, Message, VerifyEmail, SignUpComplete
 from src.api.v1.services import UserService, SignupService
 from src.utils.mail_util import mail_service
 
@@ -9,7 +9,7 @@ router = APIRouter()
 
 
 @router.get(
-    "/check_account/{account}", status_code=status.HTTP_200_OK, response_model=Message
+    "/check_account/{account}/", status_code=status.HTTP_200_OK, response_model=Message
 )
 async def check_account(
     account: EmailStr,
@@ -19,10 +19,22 @@ async def check_account(
     return Message(message="Registration confirmation code has been sent")
 
 
-@router.post("/sign-up", status_code=status.HTTP_200_OK)
+@router.post("/sign-up/", status_code=status.HTTP_200_OK)
 async def sign_up(
     email_token: VerifyEmail, signup_service: SignupService = Depends(SignupService)
 ):
     return signup_service.token_service.verify_signup_token(
         email_token.account, email_token.invite_token
     )
+
+
+@router.post(
+    "auth/api/v1/sign-up-complete/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=Message,
+)
+async def sign_up_complete(
+    data: SignUpComplete, signup_service: SignupService = Depends(SignupService)
+):
+    await signup_service.create_company_and_admin(data)
+    return Message(message="Done...")
