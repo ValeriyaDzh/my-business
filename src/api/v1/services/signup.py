81286d518eb2ae6d46ed30beb76b548aa2a084ff
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-21 12:53:23 +0300

    feat: add create_company_and_admin

--- src/api/v1/services/signup.py
+++ src/api/v1/services/signup.py
@@ -1,23 +1,25 @@
 import logging
 
-from src.api.v1.services import UserService, CompanyService
+from src.schemas.signup import SignUpComplete
 from src.utils.auth import TokenService
-from src.utils.exceptions import AlreadyExistsException
+from src.utils.exceptions import AlreadyExistsException, DatabaseException
 from src.utils.mail_util import MailService, mail_service
+from src.utils.service import BaseService
+from src.utils.unit_of_work import transaction_mode
 
 logger = logging.getLogger(__name__)
 
 
-class SignupService:
+class SignupService(BaseService):
     def __init__(self):
-        self.user_service: UserService = UserService()
-        self.company_service: CompanyService = CompanyService()
+        super().__init__()
         self.token_service: TokenService = TokenService()
         self.mail_service: MailService = mail_service
 
+    @transaction_mode
     async def check_and_send_invate(self, email: str):
 
-        if await self.user_service.get_by_email(email):
+        if await self.uow.user_repository.get_by_email(email):
             raise AlreadyExistsException(detail="This email is already in use")
 
         token = self.token_service.generate_signup_token()
@@ -28,3 +30,33 @@ class SignupService:
             self.token_service.save_signup_token(account=email, token=token)
         except Exception as e:
             logger.exception(f"Failed to send signup token: {e}")
+
+    @transaction_mode
+    async def create_company_and_admin(self, playload: SignUpComplete) -> None:
+        try:
+            if await self.uow.company_repository.get_by_field(
+                "name", playload.company_name
+            ):
+                raise AlreadyExistsException("Company with this name is already exist")
+
+            company_id = await self.uow.company_repository.add_one_and_get_id(
+                name=playload.company_name
+            )
+            logger.debug(f"Created company {playload.company_name} - {company_id}")
+
+            data_dict = playload.model_dump()
+            del data_dict["company_name"]
+            data_dict["hashed_password"] = data_dict.pop("password")
+            data_dict["email"] = data_dict.pop("account")
+            logger.debug(f"Prepared user: {data_dict}")
+
+            created_user = await self.uow.user_repository.add_one_and_get_obj(
+                **data_dict | {"is_admin": True, "company_id": company_id}
+            )
+            logger.debug(created_user)
+
+        except AlreadyExistsException:
+            raise
+        except Exception as e:
+            logger.error(f"Database error: {e}")
+            raise DatabaseException
