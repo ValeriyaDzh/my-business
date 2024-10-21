3b4e28e7a0d4c184489c87417c2201fdb8261e66
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-21 12:40:59 +0300

    add get_by_name

--- src/repositories/company.py
+++ src/repositories/company.py
@@ -1,6 +1,15 @@
+from uuid import UUID
+
 from src.models import Company
 from src.utils.repository import SqlAlchemyRepository
 
 
 class CompanyRepository(SqlAlchemyRepository):
+
     model = Company
+
+    async def create_and_get_id(self, name: str) -> UUID:
+        return await self.add_one_and_get_id(name=name)
+
+    async def get_by_name(self, name: str) -> Company | None:
+        return await self.get_by_field("name", name)
