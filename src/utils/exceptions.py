9283d903c37cf9da2578ec916a4fe34256e3845a
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-22 18:36:54 +0300

    feat: add BadRequestException

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -30,3 +30,8 @@ class UnauthorizedException(HTTPException):
 class ForbiddenException(HTTPException):
     def __init__(self, detail: str = "Access is denied"):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail=detail)
+
+
+class BadRequestException(HTTPException):
+    def __init__(self, detail: str = "Incorrect data"):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
