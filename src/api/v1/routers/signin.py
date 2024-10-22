3dc0568d50e4b869dfacf21432fda5effb027d38
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-22 13:00:17 +0300

    feat: add sign-in

---
+++ src/api/v1/routers/signin.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends, status
+
+from src.api.v1.services import SignInService
+from src.schemas.signin import SignIn, Token
+
+router = APIRouter()
+
+
+@router.post("/sign-in", status_code=status.HTTP_200_OK, response_model=Token)
+async def login_for_access_token(
+    login_data: SignIn,
+    signin_service: SignInService = Depends(SignInService),
+):
+    token = await signin_service.auth_and_create_token(
+        login_data.email, login_data.password
+    )
+    return token
+
+
+@router.get("/me")
+async def get_me():
+    return {"hello": "me"}
