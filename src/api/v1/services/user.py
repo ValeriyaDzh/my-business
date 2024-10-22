e466ca769a181029b13ae0d5577ecf926d323713
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-22 12:47:32 +0300

    feat: add get_by_id

--- src/api/v1/services/user.py
+++ src/api/v1/services/user.py
@@ -1,9 +1,15 @@
 import logging
+from uuid import UUID
 
+from src.models import User
 from src.utils.service import BaseService
+from src.utils.unit_of_work import transaction_mode
 
 logger = logging.getLogger(__name__)
 
 
 class UserService(BaseService):
-    pass
+
+    @transaction_mode
+    async def get_by_id(self, id: UUID) -> User | None:
+        return await self.uow.user_repository.get_by_field("id", id)
