d044cfa1744bfd82c1f507595d2d62f6f47f7d30
Valeriya <Dzhavadova.v@yandex.ru>
2024-10-29 14:45:14 +0300

    feat: add DepartmentUpdate

--- src/schemas/department.py
+++ src/schemas/department.py
@@ -11,3 +11,11 @@ class Department(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class DepartmentUpdate(BaseModel):
+    name: str | None = None
+    parent_id: int | None = None
+
+    class Config:
+        from_attributes = True
