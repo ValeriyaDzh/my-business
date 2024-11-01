f23167a8769a6c345dd156b1d0b83e0bae25ba8b
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-01 20:06:04 +0300

    add valid_position_admin

--- src/api/v1/dependencies/__init__.py
+++ src/api/v1/dependencies/__init__.py
@@ -1,3 +1,4 @@
-__all__ = ["is_admin"]
+__all__ = ["is_admin", "valid_position_admin"]
 
 from src.api.v1.dependencies.admin import is_admin
+from src.api.v1.dependencies.position import valid_position_admin
