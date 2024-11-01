f9aeb08d5fb5e473c7a0b580523803706c071ecc
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-01 11:50:27 +0300

    feat: add base responses

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -1,5 +1,24 @@
 from pydantic import BaseModel
+from starlette.status import HTTP_200_OK, HTTP_201_CREATED
 
 
 class Message(BaseModel):
     message: str
+
+
+class BaseResponse(BaseModel):
+    status: int = HTTP_200_OK
+    error: bool = False
+
+
+class BaseCreateResponse(BaseModel):
+    status: int = HTTP_201_CREATED
+    error: bool = False
+
+
+class Confirm(BaseModel):
+    flag: bool
+
+
+class BaseConfirmResponse(BaseResponse):
+    playload: Confirm
