b002fc5ae366fef25c9dc0b3905d26ca6e7d2d47
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-01 20:03:53 +0300

    add PositionRepository

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,5 +1,11 @@
-__all__ = ["CompanyRepository", "DepartmentRepository", "UserRepository"]
+__all__ = [
+    "CompanyRepository",
+    "DepartmentRepository",
+    "PositionRepository",
+    "UserRepository",
+]
 
 from src.repositories.company import CompanyRepository
 from src.repositories.department import DepartmentRepository
+from src.repositories.position import PositionRepository
 from src.repositories.user import UserRepository
