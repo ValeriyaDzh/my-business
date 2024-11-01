d5f6433b2f547b2c86a0436020931d510890869c
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-01 20:03:43 +0300

    add Position

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,13 @@
-__all__ = ["Base", "Company", "Department", "User"]
+__all__ = [
+    "Base",
+    "Company",
+    "Department",
+    "Position",
+    "User",
+]
 
 from src.models.base import Base
 from src.models.company import Company
 from src.models.department import Department
+from src.models.position import Position
 from src.models.user import User
