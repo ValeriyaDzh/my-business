c9a9e3410cf6eaedb4696039ba57c549e2d705bb
Valeriya <Dzhavadova.v@yandex.ru>
2024-11-06 12:08:21 +0300

    feat: add handling JWTError

--- src/middlware/auth.py
+++ src/middlware/auth.py
@@ -4,8 +4,10 @@ from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordBearer
 from starlette.middleware.base import BaseHTTPMiddleware
+from jose import JWTError
 
 from src.api.v1.services import UserService
+from src.models import User
 from src.utils.auth import TokenService
 from src.utils.exceptions import UnauthorizedException
 
@@ -30,7 +32,10 @@ class AuthMiddleware(BaseHTTPMiddleware):
                 return await call_next(request)
 
             token = await oauth2_scheme(request)
-            decoded_token = self.token_service.decode_jwt(token)
+            try:
+                decoded_token = self.token_service.decode_jwt(token)
+            except JWTError:
+                raise UnauthorizedException
 
             user_id = decoded_token.get("sub")
             company_id = decoded_token.get("company_id")
@@ -38,9 +43,10 @@ class AuthMiddleware(BaseHTTPMiddleware):
             if not user_id or not company_id:
                 raise UnauthorizedException
 
-            user = await self.user_service.get_by_id(user_id)
+            user: User = await self.user_service.get_by_id(user_id)
+            logger.debug(f"{user=}")
             if not user and user.company_id != company_id:
-                logger.debug(f"{user.id} not found or invalid token data")
+                logger.debug("Not found or invalid token data")
                 raise UnauthorizedException
 
             request.state.user = user
@@ -48,8 +54,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
             logger.debug(f"user: {request.state.user=}")
 
             return await call_next(request)
-
-        except (UnauthorizedException, HTTPException) as exc:
+        except (UnauthorizedException, HTTPException, JWTError) as exc:
             return JSONResponse(
                 status_code=exc.status_code,
                 content={"detail": exc.detail},
